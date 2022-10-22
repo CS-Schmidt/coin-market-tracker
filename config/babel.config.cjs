e99e44370cab1e5cc3a46f2c306f968bcfbc45fa
cs-schmidt <es.schmidt.cs@gmail.com>
2022-10-22 04:15:33 -0600

    build: establish core build system
    Established the core build system for the project. The build system
    leverages browserslist, babel, postcss, and webpack to manage all
    project builds for a react-based application.

---
+++ config/babel.config.cjs
@@ -0,0 +1,32 @@
+'use strict';
+
+var path = require('path');
+
+// Babel configuration function
+module.exports = function createConfig({ env, cache }) {
+  var isProduction = env('prod');
+  // Tells Babel to cache the result of the config function. This way Babel
+  // avoids recomputing the config every time a file is compiled.
+  cache.forever();
+  // Return configuration object
+  return {
+    // Plugins are configured here. Babel's code transformations are enabled by
+    // applying plugins (or presets) to your configuration file. Plugins run
+    // before presets and are loaded from last to first.
+    plugins: isProduction ? null : ['react-refresh/babel'],
+    // Presets are declared here. Each acts as a sharable set of Babel plugins
+    // and/or config options. Presets are loaded from last to first, after
+    // plugins.
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '3.22.8',
+          configPath: path.resolve('.browserslistrc'),
+        },
+      ],
+      '@babel/preset-react',
+    ],
+  };
+};
---
+++ config/postcss.config.cjs
@@ -0,0 +1,16 @@
+'use strict';
+
+// PostCSS configuration object
+module.exports = {
+  // Plugins for PostCSS. Each plugin adds functionality to PostCSS as a CSS
+  // transformation tool.
+  plugins: [
+    // The `postcss-normalize` plugin injects CSS normalize into the bundle.
+    require('postcss-normalize'),
+    // The `postcss-preset-env` plugin will use the browserslist
+    // configuration to add polyfills to CSS bundles; autoprefixer is
+    // included in postcss-preset-env.
+    require('postcss-preset-env'),
+    require('cssnano')({ preset: 'default' }),
+  ],
+};
